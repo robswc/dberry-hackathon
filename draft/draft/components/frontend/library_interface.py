f9e4f5547301dbf00adc36c4b3a2676333edd208
robswc <robert.s.w.carroll@gmail.com>
2021-03-06 13:52:35 -0500

    more framework stuffs

--- draft/draft/components/frontend/library_interface.py
+++ draft/draft/components/frontend/library_interface.py
@@ -1,24 +1,75 @@
 import dash_html_components as html
 import dash_core_components as dcc
 
-
-def render_book(book):
-    """
-    Renders a book div
-    :return:
-    """
-    html.Div()
-
-def render(library):
-    books = library.books
-
-    book_list = []
-    for book in books:
-        book_list.append(render_book(book))
+# import library
+from components.backend.library import Library
 
 
-    library = html.Div([
-        html.Ul(book_list)
-    ])
+class LibraryInterface:
+    def __init__(self):
+        self.libraries = []
 
-    return library
+    def create_library(self, title):
+        self.libraries.append(Library(title))
+
+    def get_library(self, title):
+        for lib in self.libraries:
+            if lib.title == title:
+                return lib
+
+    def render_book(self, book):
+        """
+        Renders a book div
+        :return:
+        """
+        book_html = html.Div(
+            className='card',
+            children=[
+                html.Div(
+                    className='card-horizontal',
+                    children=[
+                        html.Div(
+                            className='img-square-wrapper',
+                            children=[
+                                html.Img(
+                                    src='http://via.placeholder.com/80x100',
+                                    className=''
+                                )
+                            ]
+                        ),
+                        html.Div(
+                            className='card-body',
+                            children=[
+                                html.Div(book.title, className='card-title'),
+                                html.Div(book.author, className='card-subtitle mb-2 text-muted'),
+                            ]
+                        ),
+                    ]
+                ),
+            ]
+        )
+
+        return book_html
+
+    def render(self, library):
+
+
+
+        books = library.books
+        book_list = []
+        for book in books:
+            print('Hereeee')
+            book_list.append(self.render_book(book))
+
+        library = html.Div([
+            dcc.Dropdown(
+                id='demo-dropdown',
+                options=[
+                    {'label': 'default', 'value': 'default'},
+                ],
+                value='default'
+            ),
+            html.Ul(book_list)
+        ])
+
+        return library
--- draft/draft/pages/_import.py
+++ draft/draft/pages/_import.py
@@ -9,14 +9,27 @@ from components.frontend import renders
 # Interfaces
 from components.frontend import image_upload_interface
 from components.frontend import input_interface
+from components.frontend.library_interface import LibraryInterface
+
+# Book
+from components.backend.book import Book
 
 img_upload = image_upload_interface.render()
 input_module = input_interface.render()
+li = LibraryInterface()
+
+# Make library
+li.create_library('default')
+li.get_library('default').add_book(Book('Harry Potter', 'JK Rowling', '10101'))
+li.get_library('default').add_book(Book('Harry Potter 2', 'JK Rowling', '10101'))
+li.get_library('default').add_book(Book('Harry Potter 3', 'JK Rowling', '10101'))
+li.get_library('default').add_book(Book('Harry Potter 4', 'JK Rowling', '10101'))
+li.get_library('default').add_book(Book('Harry Potter 5', 'JK Rowling', '10101'))
 
 tiles = [
     renders.render_tile(img_upload, 'Image', 4),
     renders.render_tile(input_module, 'Input', 4),
-    renders.render_tile(html.Div('Library'), 'Library', 4)
+    renders.render_tile(li.render(li.get_library('default')), 'Library', 4)
 ]
 
 layout = html.Div(
