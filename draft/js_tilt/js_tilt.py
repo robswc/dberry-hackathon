234396bbccb43d650e885b2a252136a723614a84
brandonfrulla <bfrulla@mail.umw.edu>
2021-03-06 17:58:21 -0500

    modified:   js_tilt.py

--- draft/js_tilt/js_tilt.py
+++ draft/js_tilt/js_tilt.py
@@ -1,50 +1,31 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-
-
-
-import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
 from flask import send_from_directory
+import os
 
-# #including tilt.js
-external_scripts = [
-    'https://cdnjs.cloudflare.com/ajax/libs/tilt.js/1.2.1/tilt.jquery.min.js',
-    'https://unpkg.com/tilt.js@1.2.1/dest/tilt.jquery.min.js'
-]
-
-app = dash.Dash(__name__, external_scripts=external_scripts)
-
-# app.head = [html.Link(rel='stylesheet', href='/assets/css/stylesheet.css')]
+assets_path = os.getcwd() +'/assets'
+app = dash.Dash(__name__, assets_folder=assets_path)
 
 app.layout = html.Div([
     
-    html.Div(
-        className="app-header",
-        children=[
-            html.Div('d.berry tilt', className="app-header--title")
-        ]
-    ),
+    # html.Div(
+    #     className="app-header",
+    #     children=[
+    #         html.Div('d.berry Tilt.js', className="app-header--title")
+    #     ]
+    # ),
 
-    html.Div([
-        html.Div(
-            children=html.Div([
-                html.Div('''
-                    I will make tilt work with Dash if it kills me
-                ''')
-            ])
-        ),
-
-        html.Div(
-            html.Img(
-                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FI%2F4193iI6WHqL._SY344_BO1%2C204%2C203%2C200_.jpg&f=1&nofb=1"
-            )
-        )
-    ])
+    # html.Div([
+    #     html.Div(
+    #         children=html.Div([
+    #             html.Div('''
+    #                 I will make tilt work with Dash if it kills me
+    #             ''')
+    #         ])
+    #     )
+    # ])
 ])
 
 
