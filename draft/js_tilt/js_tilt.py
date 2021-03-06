dedf58f06952217ed419c50302aaa44e1ae09796
brandonfrulla <bfrulla@mail.umw.edu>
2021-03-06 17:14:16 -0500

    new file:   assets/css/stylesheet.css 	new file:   assets/css/typography.css 	new file:   assets/index.html 	new file:   assets/js/tilt.jquery.js 	modified:   js_tilt.py

--- draft/js_tilt/js_tilt.py
+++ draft/js_tilt/js_tilt.py
@@ -4,18 +4,23 @@ import dash_html_components as html
 
 app = dash.Dash(__name__)
 
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+from flask import send_from_directory
+
+app.head = [html.Link(rel='stylesheet', href='/assets/css/stylesheet.css')]
+
 app.layout = html.Div([
-    html.Div(
-        className="app-header",
-        children=[
-            html.Div('Plotly Dash', className="app-header--title")
-        ]
-    ),
-    
-    html.Img(
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.FSEs7JbMmmOvXa8UFA9a2QAAAA%26pid%3DApi&f=1"
-    )
 ])
+
+
+@app.server.route("/assets/css")
+def static_file(path):
+    static_folder = os.path.join(os.getcwd(), 'assests')
+    return send_from_directory(static_folder, path)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
