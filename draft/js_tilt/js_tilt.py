98b89e60d31356394100c6490c73aeac97f122e6
brandonfrulla <51884427+brandonfrulla@users.noreply.github.com>
2021-03-06 17:57:54 -0500

    Update js_tilt.py

--- draft/js_tilt/js_tilt.py
+++ draft/js_tilt/js_tilt.py
@@ -1,10 +1,4 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-
-
-
-import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
